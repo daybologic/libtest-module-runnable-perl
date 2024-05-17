abe2e93c2b07a7539debe0eb5654402452f7fe5e
Rev. Duncan Ross Palmer <2e0eol@gmail.com>
2024-05-17 23:43:37 +0100

    📝 Move PoD for new methods into primary package

--- lib/Test/Module/Runnable/Base.pm
+++ lib/Test/Module/Runnable/Base.pm
@@ -186,19 +186,7 @@ sub unique {
 
 =item C<uniqueStr([$length])>
 
-Return a unique alphanumeric string which shall not be shorter than the specified C<$length>,
-which is 1 by default.  The string is guaranteed to evaluate true in a boolean context.
-
-The numerical value of each character is obtained from L</unique>.
-
-Note that the strings returned from this function are B<only> guaranteed to
-be in monotonically increasing lexicographical order if they are all of
-the same length.  Therefore if this is a concern, specify a length which
-will be long enough to include all the strings you wish to generate,
-for example C<uniqueStr(4)> would produce C<62**4> (over 14 million)
-strings in increasing order.
-
-Can be called statically and exported in the same way as L</unique>.
+See L<Test::Module::Runnable/uniqueStr($length)>
 
 =cut
 
@@ -209,10 +197,7 @@ sub uniqueStr {
 
 =item C<uniqueStrCI($length)>
 
-Works exactly the same as L</uniqueStr([$length])> except that the results are case
-sensitively identical.  Note that the strings are not guaranteed to be
-all lowercase or all uppercase, you may get "A" or "a", but you will
-never get both.  No assumption should be made about the case.
+See L<Test::Module::Runnable/uniqueStrCI($length)>
 
 =cut
 
@@ -223,24 +208,7 @@ sub uniqueStrCI {
 
 =item C<uniqueDomain([$options])>
 
-Returns a unique, fake domain-name.  No assumptions should be made about the domain
-name or TLD returned, except that this domain cannot be registered via a domain registrar, is lower-case and is
-unique per test suite run.
-
-The optional C<$options>, if specified, must be a C<HASH> ref, and it may contain the following keys:
-
-=over
-
-=item C<length>
-
-The length of the first part of the hostname.  This ensures correct lexicographic ordering.
-
-=item C<lettersOnly>
-
-Ensure that hostname parts only contain letters, not numbers.  This is also useful to
-ensure correct lexicographic ordering.
-
-=back
+See L<Test::Module::Runnable/uniqueDomain([$options])>
 
 =cut
 
@@ -259,16 +227,7 @@ sub uniqueDomain {
 
 =item C<uniqueLetters($length)>
 
-Return a unique string containing letters only, which shall not be shorter
-than the specified C<$length>, which is 1 by default.  The string is guaranteed
-to evaluate true in a boolean context.
-
-Note that the strings returned from this function are B<only> guaranteed to
-be in monotonically increasing lexicographical order if they are all of
-the same length.  Therefore if this is a concern, specify a length which
-will be long enough to include all the strings you wish to generate,
-for example C<uniqueStr(4)> would produce C<62**4> (over 14 million)
-strings in increasing order.
+See L<Test::Module::Runnable/uniqueLetters($length)>
 
 =cut
 
